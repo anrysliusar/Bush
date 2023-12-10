93dfd9e4a9fb17a26852e8e6b18b97d7acd1b418
Andrew Sliusarenko <assliusarenko@gmail.com>
2023-12-10 16:45:46 +0200

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,10 @@
+export LANG=C.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+apt-get update
+apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option DPkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install openmediavault
